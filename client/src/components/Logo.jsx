4d1934f24aa4d64fd01fe626d37a48ed8a329743
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-01-13 22:57:36 +0100

    Frontend update+ Navbar

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -3,25 +3,23 @@ import React from "react";
 import styled from "styled-components";
 
 const Logo = () => {
-	return (
-		<Container>
-			<span>Bibliophile</span> Land
-		</Container>
-	);
+  return (
+    <Container>
+      <span>Bibliophile</span> Land
+    </Container>
+  );
 };
 
 export default Logo;
 
 const Container = styled.h1`
-	cursor: pointer;
-	margin-top: 2%;
-	margin-left: 10%;
-	font-size: 28;
-	color: #d9d9d9;
-	font-weight: 600;
+  cursor: pointer;
+  font-size: 28px;
+  color: #d9d9d9;
+  font-weight: 600;
 
-	span {
-		color: #cbcbcb;
-		font-weight: 400;
-	}
+  span {
+    color: #cbcbcb;
+    font-weight: 400;
+  }
 `;
