a8b82c9909c95d1d92e0b3edddce6a413a603cef
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-01-14 17:49:06 +0100

    left side of the home page

--- client/src/components/home/Profile.jsx
+++ client/src/components/home/Profile.jsx
@@ -1,63 +1,181 @@
 import styled from "styled-components";
 
-const Profile = () => {
+const ProfileSection = () => {
   return (
     <Container>
       <ProfileInfo>
         <Profile>
-          <Photo></Photo>
+          <Photo>
+            <img src="/assets/obito.jpg" alt="" />
+          </Photo>
           <Info>
-            <Name></Name>
-            <Username></Username>
-            <Followers></Followers>
+            <Name>Moussaab</Name>
+            <Username>@marcos</Username>
+            <Followers>Following: 69</Followers>
           </Info>
         </Profile>
         <Readbooks>
-          <Title></Title>
-          <Number></Number>
+          <Title>Books Read</Title>
+          <Number>69</Number>
         </Readbooks>
         <Readbooks>
-          <Title></Title>
-          <Number></Number>
+          <Title>Books Read</Title>
+          <Number>69</Number>
         </Readbooks>
         <Readbooks>
-          <Title></Title>
-          <Number></Number>
+          <Title>Books Read</Title>
+          <Number>69</Number>
         </Readbooks>
       </ProfileInfo>
-      <FavBooks>
-        <h3></h3>
-        <Books>
-          <Book></Book>
-          <Book></Book>
-          <Book></Book>
-        </Books>
-      </FavBooks>
-      <OtherBooks>
-        <h3></h3>
-        <Books>
-          <Book></Book>
-          <Book></Book>
-          <Book></Book>
-        </Books>
-      </OtherBooks>
+      <BooksSection>
+        <FavBooks>
+          <span>Favorite Books</span>
+          <Books>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+          </Books>
+        </FavBooks>
+        <OtherBooks>
+          <span>Other Books</span>
+          <Books>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+            <Book>
+              <img src="/assets/book.jpg" />
+              <BookTitle>Memories</BookTitle>
+            </Book>
+          </Books>
+        </OtherBooks>
+      </BooksSection>
     </Container>
   );
 };
 
-export default Profile;
+export default ProfileSection;
 
-const Container = styled.div``;
-const ProfileInfo = styled.div``;
-const Photo = styled.div``;
-const Info = styled.div``;
-const Name = styled.div``;
-const Username = styled.div``;
-const Followers = styled.div``;
-const Readbooks = styled.div``;
-const Title = styled.div``;
-const Number = styled.div``;
-const FavBooks = styled.div``;
-const Books = styled.div``;
-const Book = styled.div``;
-const OtherBooks = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 1.5rem;
+
+  height: 100%;
+  width: 100%;
+`;
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 0.7rem;
+`;
+const Profile = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: space-between;
+  width: 100%;
+  padding-left: 2.4rem;
+  padding-top: 1.3rem;
+`;
+const Photo = styled.div`
+  width: 100px;
+  height: 100px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+`;
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-self: flex-end;
+  margin-right: auto;
+  margin-left: 0.8rem;
+`;
+const Name = styled.span`
+  font-size: 18px;
+`;
+const Username = styled.span`
+  font-size: 15px;
+`;
+const Followers = styled.span`
+  font-size: 13px;
+`;
+
+const Readbooks = styled.div`
+  width: 87%;
+  height: 45px;
+  background-color: goldenrod;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+`;
+const Title = styled.span``;
+const Number = styled.span``;
+
+/************** */
+
+const BooksSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+const FavBooks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.7rem;
+`;
+
+const Books = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+const Book = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-direction: column;
+
+  img {
+    width: 100px;
+    height: 170px;
+  }
+  * {
+    cursor: pointer;
+  }
+`;
+const BookTitle = styled.span`
+  font-size: 12px;
+`;
+const OtherBooks = styled(FavBooks)``;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,40 @@
 import styled from "styled-components";
+import ProfileSection from "../components/home/Profile";
 
 const Home = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Left>
+        <ProfileSection />
+      </Left>
+      <Right>Marcos Work Here!</Right>
+    </Container>
+  );
 };
 
 export default Home;
 
 const Container = styled.main`
-  min-height: 90vh;
+  height: 830px;
   width: 100%;
+  display: flex;
+  margin-top: 1.7rem;
+  margin-bottom: 1.7rem;
+  gap: 1rem;
+`;
+
+const Left = styled.section`
+  width: 400px;
+  height: 100%;
+  background-color: #141a1f;
+  border-radius: 10px;
+  padding: 1.5rem;
+  flex: 1;
+`;
+const Right = styled.section`
+  height: 100%;
+  background-color: #141a1f;
+  border-radius: 10px;
+  padding: 1.5rem;
+  flex: 2.5;
 `;
