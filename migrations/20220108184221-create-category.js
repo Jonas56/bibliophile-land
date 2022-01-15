12840b446d497b4d3376c8fc0b9c584f1f54ca3d
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-01-15 21:26:43 +0100

    Update migrations

--- migrations/20220108184221-create-category.js
+++ migrations/20220108184221-create-category.js
@@ -11,6 +11,7 @@ module.exports = {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
       },
       created_at: {
         allowNull: false,
--- migrations/20220108185653-create-author.js
+++ migrations/20220108185653-create-author.js
@@ -11,6 +11,7 @@ module.exports = {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
       },
       biography: {
         type: DataTypes.TEXT,
