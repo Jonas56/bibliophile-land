436ab6941c90dbc3e2a27d83cd84bcbc73ceb7c3
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-02-13 21:24:01 +0100

    book info part

--- client/src/pages/Book.jsx
+++ client/src/pages/Book.jsx
@@ -4,7 +4,23 @@ import styled from "styled-components";
 const Book = () => {
   return (
     <Container>
-      <BookInfo></BookInfo>
+      <BookInfo>
+        <Info>
+          <div className="book-image">
+            <img src="./assets/book.jpg" alt="" />
+          </div>
+          <div className="book-info">
+            <div className="book-info__title">Book </div>
+            <p className="book-info__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Recusandae non eius voluptatum ab reiciendis consequatur possimus
+              sed optio repudiandae quisquam.
+            </p>
+            <div className="book-info__buttons">btn</div>
+          </div>
+        </Info>
+        <Details>test</Details>
+      </BookInfo>
       <Suggetions></Suggetions>
     </Container>
   );
@@ -21,12 +37,44 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(5, 1fr);
+
+  @media screen and (max-width: 1280px) {
+    display: flex;
+    gap: 1.5rem;
+  }
 `;
 const BookInfo = styled.div`
   grid-area: 1 / 1 / 6 / 3;
-  background-color: white;
   width: 100%;
   height: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(2, 1fr);
+`;
+const Info = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 2.2rem;
+
+  .book-image {
+    grid-area: 1 / 1 / 4 / 2;
+    img {
+      width: 237px;
+      height: 359px;
+    }
+  }
+
+  .book-info {
+    grid-area: 1 / 2 / 4 / 4;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+  }
+`;
+const Details = styled.div`
+  grid-area: 2 / 1 / 3 / 2;
 `;
 const Suggetions = styled.div`
   grid-area: 1 / 3 / 6 / 4;
