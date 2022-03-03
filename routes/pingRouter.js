dcb3b086492449e6bc1ebc7d6c991ae37d087dab
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-03-03 18:07:48 +0100

    Update .gitignore

--- routes/pingRouter.js
+++
@@ -1,8 +0,0 @@
-const pingRouter = require("express").Router();
-const httpGetPing = require("../controllers/ping");
-
-pingRouter.get("/", (req, res) => {
-  return httpGetPing.getAll(req, res);
-});
-
-module.exports = pingRouter;
