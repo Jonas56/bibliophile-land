d94cbc68191c39580f48d9eaf0cded9aa16a719d
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-03-04 16:35:54 +0100

    Update api routes

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,9 @@
 const api = require("express").Router();
-const loginRouter = require("./loginRouter");
 const registerRouter = require("./registerRouter");
 const booksRouter = require("./booksRouter");
 const authorsRouter = require("./authorsRouter");
 const collectionRouter = require("./collectionRouter");
 
-api.use("/api/login", loginRouter);
 api.use("/api/register", registerRouter);
 api.use("/api/books", booksRouter);
 api.use("/api/authors", authorsRouter);
