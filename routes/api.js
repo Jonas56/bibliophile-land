45c1047f8fcfd03e27d4f4e50fde4f6f6063f284
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-03-04 16:23:36 +0100

    Add register route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,14 @@
 const api = require("express").Router();
+const loginRouter = require("./loginRouter");
+const registerRouter = require("./registerRouter");
 const booksRouter = require("./booksRouter");
 const authorsRouter = require("./authorsRouter");
 const collectionRouter = require("./collectionRouter");
 
+api.use("/api/login", loginRouter);
+api.use("/api/register", registerRouter);
 api.use("/api/books", booksRouter);
 api.use("/api/authors", authorsRouter);
-api.use("/api/users/", collectionRouter);
+api.use("/api/users", collectionRouter);
 
 module.exports = api;
