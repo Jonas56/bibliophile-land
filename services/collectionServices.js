9c3f9c45c44762318a27354f90fcf61ee193bec9
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-03-05 19:18:42 +0100

    Update collection controller

--- services/collectionServices.js
+++ services/collectionServices.js
@@ -1,15 +1,18 @@
-const { Book, User } = require("../models");
+const { Book, User, UserBooks } = require("../models");
 
 const httpGetAllUserBooks = async (req, res) => {
-  const id = Number(req.params.userid);
+  // TODO: Extract id from req middleware
+  const id = 1;
   const userBooks = await User.findByPk(id, {
     include: {
       model: Book,
       as: "books",
+      attributes: ["id", "title", "image_link", "isbn"],
       through: {
         attributes: [],
       },
     },
+    attributes: ["id", "username", "name"],
   });
   if (userBooks) {
     return res.status(200).json(userBooks);
@@ -18,4 +21,53 @@ const httpGetAllUserBooks = async (req, res) => {
   }
 };
 
-module.exports = { httpGetAllUserBooks };
+const httpAddBookToCollection = async (req, res) => {
+  // TODO: Extract id from middleware
+  const userId = 1;
+  const bookId = Number(req.params.bookid);
+
+  const user = await User.findByPk(userId);
+
+  const book = await Book.findByPk(bookId);
+
+  if (!(user && book)) {
+    return res.status(400).json({ message: "Cannot find the provided book!" });
+  }
+
+  const associate = await user.addBooks(book, { through: { UserBooks } });
+
+  if (associate[0]) {
+    return res.status(200).json({ message: "Book added succesfully!" });
+  } else {
+    return res.status(400).json({ error: "Book already added!" });
+  }
+};
+
+const httpRemoveFromCollection = async (req, res) => {
+  // TODO: Extract id from middleware
+  const userId = 1;
+  const bookId = Number(req.params.bookid);
+
+  const user = await User.findByPk(userId);
+
+  const book = await Book.findByPk(bookId);
+
+  if (!(user && book)) {
+    return res.status(400).json({ message: "Cannot find the provided book" });
+  }
+
+  const associate = await user.removeBooks(book, { through: { UserBooks } });
+
+  console.log("Association", associate);
+  if (associate) {
+    return res.status(200).json({ message: "Book removed succesfully!" });
+  } else {
+    return res.status(400).json({ error: "Book already removed!" });
+  }
+};
+
+module.exports = {
+  httpGetAllUserBooks,
+  httpAddBookToCollection,
+  httpRemoveFromCollection,
+};
