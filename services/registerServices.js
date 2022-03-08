6e2bccc1e82753c12cba52f3b42364cf8f81e96d
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-03-08 18:45:20 +0100

    Implement error handling and form validation for signup route

--- services/registerServices.js
+++ services/registerServices.js
@@ -1,20 +1,7 @@
 const { User } = require("../models");
 
-const vlaidatePassword = (value) => {
-  const validPassword =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-  return validPassword.test(value);
-};
-
 const httpHanldeRegister = async (req, res) => {
   const body = req.body;
-
-  if (!body.password || !vlaidatePassword(body.password)) {
-    return res.status(400).json({
-      error:
-        "Password must contain minimum eight characters, at least one letter, one number and one special character",
-    });
-  }
   const createdUser = await User.create({
     name: body.name,
     email: body.email,
@@ -23,7 +10,10 @@ const httpHanldeRegister = async (req, res) => {
     age: body.age,
   });
 
-  return res.status(201).json(createdUser);
+  return res.status(201).json({
+    username: createdUser.username,
+    mesagge: "You have succesfully signup!",
+  });
 };
 
 module.exports = { httpHanldeRegister };
