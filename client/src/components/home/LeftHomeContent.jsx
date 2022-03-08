2c631a18f8f8e09dd54a642105445901b933c943
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-03-08 13:36:45 +0100

    new files

---
+++ client/src/components/home/LeftHomeContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftHomeContent = () => {
+  return <div>LeftHomeContent</div>;
+};
+
+export default LeftHomeContent;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Logo from "../Logo";
+import Logo from "../components/Logo";
 
 const Login = () => {
   return (
