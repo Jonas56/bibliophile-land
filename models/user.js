d00631986cf4a53954f2bc0748cde0a9034a57c8
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-03-14 19:00:35 +0100

    Update user <-> book models Many-to-many relationship

--- models/user.js
+++ models/user.js
@@ -9,12 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Book, UserBooks }) {
+    static associate({ Book, UserCollection }) {
       // define association here
       this.belongsToMany(Book, {
-        through: UserBooks,
+        through: UserCollection,
         foreignKey: "user_id",
-        as: "books",
+        as: "collection",
       });
     }
   }
