45a55c9e403f32e932e2625ffb75e9e09e74e17f
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-03-16 16:41:38 +0100

    Implement reading api routes

--- routes/api.js
+++ routes/api.js
@@ -4,11 +4,13 @@ const registerRouter = require("./registerRouter");
 const booksRouter = require("./booksRouter");
 const authorsRouter = require("./authorsRouter");
 const collectionRouter = require("./collectionRouter");
+const readingRouter = require("./readingRouter");
 
 api.use("/api/login", loginRouter);
 api.use("/api/register", registerRouter);
 api.use("/api/books", booksRouter);
 api.use("/api/authors", authorsRouter);
 api.use("/api/collection", collectionRouter);
+api.use("/api/reading", readingRouter);
 
 module.exports = api;
