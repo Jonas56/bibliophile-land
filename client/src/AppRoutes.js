d14533a69aec56b8104bde4b03f86b584110f5b7
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-03-18 21:18:52 +0100

    new routes

--- client/src/AppRoutes.js
+++ client/src/AppRoutes.js
@@ -3,6 +3,7 @@ import {
     Route
   } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import PageLayout1 from "./components/PageLayout1";
 import BookPage from "./pages/BookPage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -13,10 +14,12 @@ const AppRoutes = () => {
   <Routes>
       <Route path="/" element={<Navbar />}>
         {/* home page */ }
-        <Route index element={<Home />} />
+        <Route index element={<PageLayout1 />} />
+
         {/* login and signup */ }
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Login />} />
+
         {/* books */}
         <Route path="books" element={<BookPage />}>
           <Route path=":bookid" element={<BookPage />}>
@@ -24,16 +27,27 @@ const AppRoutes = () => {
             <Route path="toberead" element={<BookPage />}/>
           </Route>
         </Route>
+
         {/* authors */}
         <Route path="authors" element={<BookPage />}>
           <Route path=":authorid" element={<BookPage />}>
           </Route>
         </Route>
+
         {/* user */}
-        <Route path="user" element={<UserPage />}></Route>
+        <Route path="user" element={<UserPage />}/>
+
+        {/* Categories */}
+        <Route path="category" >
+          <Route path=":bookid" />
+        </Route>
+
+        {/* Search */}
+        <Route path="search" />
 
         {/* Not Found */}
         <Route path="*" element={<p>NotFound</p>} />
+        
       </Route>
   </Routes>
   )
