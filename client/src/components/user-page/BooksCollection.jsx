533c96e3233ed525167aae7da896639f87daab98
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-04-10 00:37:22 +0000

    update user page

--- client/src/components/user-page/BooksCollection.jsx
+++ client/src/components/user-page/BooksCollection.jsx
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import Book from "../home/BookSliderCard";
 
 const BooksCollection = ({ title, books }) => {
-  return books.status === "loading" ? (
+  return books?.status === "loading" ? (
     <p>Loading..</p>
   ) : (
     <Container>
       <span className="read-books_title">{title}</span>
       <Books>
         {" "}
-        {books.list.map((book, index) => (
+        {books?.list.map((book, index) => (
           <Book key={index} book={book} />
         ))}
       </Books>
@@ -22,12 +22,17 @@ export default BooksCollection;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   gap: 1rem;
 
   .read-books_title {
     font-weight: 500;
     font-size: 16px;
     line-height: 20px;
+  }
+
+  @media screen and (max-width: 1280px) {
+    align-items: center;
   }
 `;
 
